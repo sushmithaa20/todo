d1d94a6404de9d0a7b30eefa4db637e983d3d544
sushmitha-sys <sushmitha.20.2001.com>
2022-07-15 10:04:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const item=document.querySelector("#item");
+const toDoBox=document.querySelector("#to-do-box");
+
+
+const saveNotes = () =>{
+    const notes = document.querySelectorAll(".notes texrarea");
+    const data = [];
+    notes.forEach(
+        (note)=> {
+            data.push(note)
+        }
+    )
+}
+
+item.addEventListener(
+    "keyup",
+    function(event){
+        if(event.key == "Enter"){
+            addToDo(this.value)
+            this.value=""
+        }
+    }
+)
+const addToDo = (item) =>{
+    const listItem = document.createElement("li");
+    listItem.innerHTML= `
+        ${item}
+        <i class="fas fa-times"></i>
+    `;
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove()
+        }
+    )
+   
+    toDoBox.appendChild(listItem)
+}
